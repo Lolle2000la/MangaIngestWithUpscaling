8dfd6b2e29d16ba3eadb3572d5e4ca87f0dca2bb
Luca Auer <lolle2000.la@gmail.com>
2025-01-25 22:02:54 +0100

    Add upscaling queue

---
+++ MangaIngestWithUpscaling/Data/LibraryManagement/UpscalingQueueEntry.cs
@@ -0,0 +1,18 @@
+﻿namespace MangaIngestWithUpscaling.Data.LibraryManagement
+{
+    /// <summary>
+    /// Represents an entry in the upscaling queue, referencing a chapter and
+    /// an upscaler configuration, along with the time it was queued.
+    /// </summary>
+    public class UpscalingQueueEntry
+    {
+        public int Id { get; set; }
+        public int ChapterId { get; set; }
+        public Chapter Chapter { get; set; }
+
+        public int UpscalerConfigId { get; set; }
+        public UpscalerConfig UpscalerConfig { get; set; }
+
+        public DateTime QueuedAt { get; set; }
+    }
+}
