71040036730f86e89a9d03c17eafed8697fa98fa
Luca Auer <lolle2000.la@gmail.com>
2025-01-28 00:40:18 +0100

    Improve library model

--- MangaIngestWithUpscaling/Data/LibraryManagement/Library.cs
+++ MangaIngestWithUpscaling/Data/LibraryManagement/Library.cs
@@ -7,9 +7,13 @@
     public class Library
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string IngestPath { get; set; }
         public string NotUpscaledLibraryPath { get; set; }
-        public string UpscaledLibraryPath { get; set; }
+        public string? UpscaledLibraryPath { get; set; }
+
+        public int UpscalerConfigId { get; set; }
+        public UpscalerConfig UpscalerConfig { get; set; }
 
         public List<Manga> MangaSeries { get; set; } = new List<Manga>();
         public List<LibraryFilterRule> FilterRules { get; set; } = new List<LibraryFilterRule>();
