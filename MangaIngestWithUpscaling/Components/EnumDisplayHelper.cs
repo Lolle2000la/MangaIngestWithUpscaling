69d6c2c309a9d0ec5e7a3ce843cc08f036091fc5
Luca Auer <lolle2000.la@gmail.com>
2025-02-02 22:26:34 +0100

    Add creation of upscaler profiles

---
+++ MangaIngestWithUpscaling/Components/EnumDisplayHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace MangaIngestWithUpscaling.Components;
+
+public static class EnumDisplayHelper
+{
+    public static string GetDisplayName(this Enum enumValue)
+    {
+        var displayAttribute = enumValue.GetType()
+                                        .GetMember(enumValue.ToString())
+                                        .First()
+                                        .GetCustomAttribute<DisplayAttribute>();
+
+        return displayAttribute?.Name ?? enumValue.ToString();
+    }
+}
