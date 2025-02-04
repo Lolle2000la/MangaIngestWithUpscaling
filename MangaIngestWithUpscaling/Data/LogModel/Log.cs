59ff6d9a30b1d90a9a4f0a02e6939874d2b6321c
Luca Auer <lolle2000.la@gmail.com>
2025-02-04 23:45:32 +0100

    Add accidentally excluded Log-model file

---
+++ MangaIngestWithUpscaling/Data/LogModel/Log.cs
@@ -0,0 +1,11 @@
+﻿namespace MangaIngestWithUpscaling.Data.Logs;
+
+public class Log
+{
+    public int Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string Level { get; set; }
+    public string Exception { get; set; }
+    public string RenderedMessage { get; set; }
+    public string Properties { get; set; }
+}
