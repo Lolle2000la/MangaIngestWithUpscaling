b1d8ccd1320aab1cfebab9919f16fdbce2b92f5f
Luca Auer <lolle2000.la@gmail.com>
2025-03-11 19:47:00 +0100

    Make the mount points for the library paths in kavita configurable.

--- MangaIngestWithUpscaling/Data/LibraryManagement/Library.cs
+++ MangaIngestWithUpscaling/Data/LibraryManagement/Library.cs
@@ -1,4 +1,6 @@
-﻿namespace MangaIngestWithUpscaling.Data.LibraryManagement;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MangaIngestWithUpscaling.Data.LibraryManagement;
 
 /// <summary>
 /// Represents a library with paths for ingesting, storing not-upscaled,
@@ -11,6 +13,7 @@ public class Library
     public string IngestPath { get; set; }
     public string NotUpscaledLibraryPath { get; set; }
     public string? UpscaledLibraryPath { get; set; }
+    public KavitaLibraryConfig KavitaConfig { get; set; } = new KavitaLibraryConfig();
 
     public bool UpscaleOnIngest { get; set; }
     public int? UpscalerProfileId { get; set; }
@@ -18,4 +21,19 @@ public class Library
 
     public List<Manga> MangaSeries { get; set; } = [];
     public List<LibraryFilterRule> FilterRules { get; set; } = [];
+}
+
+[ComplexType]
+public class KavitaLibraryConfig
+{
+    /// <summary>
+    /// The mount point for the folder with the originals in Kavita.
+    /// This may differ from the normal path due to the usage of different mount points across containers.
+    /// </summary>
+    public string? NotUpscaledMountPoint { get; set; }
+    /// <summary>
+    /// The mount point for the folder with the upscaled images in Kavita.
+    /// This may differ from the normal path due to the usage of different mount points across containers.
+    /// </summary>
+    public string? UpscaledMountPoint { get; set; }
 }
---
+++ MangaIngestWithUpscaling/Data/Migrations/20250311184623_KavitaMountPointConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MangaIngestWithUpscaling.Migrations
+{
+    /// <inheritdoc />
+    public partial class KavitaMountPointConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "KavitaConfig_NotUpscaledMountPoint",
+                table: "Libraries",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "KavitaConfig_UpscaledMountPoint",
+                table: "Libraries",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KavitaConfig_NotUpscaledMountPoint",
+                table: "Libraries");
+
+            migrationBuilder.DropColumn(
+                name: "KavitaConfig_UpscaledMountPoint",
+                table: "Libraries");
+        }
+    }
+}
