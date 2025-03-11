e7475a33efdce64ea393b7e7da464d603b4b7e3b
Luca Auer <lolle2000.la@gmail.com>
2025-03-11 20:40:50 +0100

    Configure default base path for kavita

--- MangaIngestWithUpscaling/Configuration/KavitaConfiguration.cs
+++ MangaIngestWithUpscaling/Configuration/KavitaConfiguration.cs
@@ -4,6 +4,6 @@ public class KavitaConfiguration
 {
     public const string Position = "Kavita";
 
-    public string? BaseUrl { get; set; }
+    public string BaseUrl { get; set; } = "http://localhost:5000";
     public string? ApiKey { get; set; }
 }
