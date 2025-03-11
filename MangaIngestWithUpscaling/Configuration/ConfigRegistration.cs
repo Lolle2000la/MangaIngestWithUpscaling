7b307bc24f97cd24d3ea317c31c9fc9021b095ad
Luca Auer <lolle2000.la@gmail.com>
2025-03-11 20:02:35 +0100

    Allow configuration of kavita

--- MangaIngestWithUpscaling/Configuration/ConfigRegistration.cs
+++ MangaIngestWithUpscaling/Configuration/ConfigRegistration.cs
@@ -5,5 +5,6 @@ public static class ConfigRegistration
     public static void RegisterConfig(this WebApplicationBuilder builder)
     {
         builder.Services.Configure<UpscalerConfig>(builder.Configuration.GetSection(UpscalerConfig.Position));
+        builder.Services.Configure<KavitaConfiguration>(builder.Configuration.GetSection(KavitaConfiguration.Position));
     }
 }
---
+++ MangaIngestWithUpscaling/Configuration/KavitaConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace MangaIngestWithUpscaling.Configuration;
+
+public class KavitaConfiguration
+{
+    public const string Position = "Kavita";
+
+    public string? BaseUrl { get; set; }
+    public string? ApiKey { get; set; }
+}
