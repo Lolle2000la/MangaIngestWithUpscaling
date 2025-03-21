8246b4cd28a0e8fe1ac5d8144b90b0535bc2f96d
Luca Auer <lolle2000.la@gmail.com>
2025-03-21 18:41:14 +0100

    Add basic server side gRPC skeleton

---
+++ MangaIngestWithUpscaling/Api/UpscalingDistributionService.cs
@@ -0,0 +1,35 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using MangaIngestWithUpscaling.Services.BackqroundTaskQueue;
+using Upscaling;
+
+namespace MangaIngestWithUpscaling.Api;
+
+public partial class UpscalingDistributionService(
+    TaskQueue taskQueue) : UpscalingService.UpscalingServiceBase
+{
+    public override async Task GetCbzFile(CbzToUpscaleRequest request, IServerStreamWriter<CbzFileChunk> responseStream, ServerCallContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<KeepAliveResponse> KeepAlive(KeepAliveRequest request, ServerCallContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<CbzFileChunk> RequestCbzFileChunk(CbzFileChunkRequest request, ServerCallContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<UpscaleTaskDelegationResponse> RequestUpscaleTask(Empty request, ServerCallContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<Empty> UploadUpscaledCbzFile(IAsyncStreamReader<CbzFileChunk> requestStream, ServerCallContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
