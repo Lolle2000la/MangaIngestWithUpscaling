0170d7fdc3b667608f683f503087f7038f531f6a
Luca Auer <lolle2000.la@gmail.com>
2025-03-26 20:54:24 +0100

    Initialize project used for the remote worker

---
+++ MangaIngestWithUpscaling.RemoteWorker/Program.cs
@@ -0,0 +1,33 @@
+using MangaIngestWithUpscaling.RemoteWorker.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    if (OperatingSystem.IsWindows())
+    {
+        serverOptions.ListenNamedPipe("MIWURemoteWorker");
+    }
+    else
+    {
+        var socketPath = Path.Combine(Path.GetTempPath(), "miwu-remote.tmp");
+        serverOptions.ListenUnixSocket(socketPath);
+    }
+
+    serverOptions.ConfigureEndpointDefaults(listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
