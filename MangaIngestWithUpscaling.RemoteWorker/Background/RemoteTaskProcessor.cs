4b867e59240da7e692b0f3b228c63e64fd573558
Luca Auer <lolle2000.la@gmail.com>
2025-03-26 23:00:22 +0100

    Add the logic for requesting a task

--- MangaIngestWithUpscaling.RemoteWorker/Background/RemoteTaskProcessor.cs
+++ MangaIngestWithUpscaling.RemoteWorker/Background/RemoteTaskProcessor.cs
@@ -1,4 +1,6 @@
-﻿using MangaIngestWithUpscaling.Api.Upscaling;
+﻿using Google.Protobuf.WellKnownTypes;
+using MangaIngestWithUpscaling.Api.Upscaling;
+using MangaIngestWithUpscaling.Shared.Data.LibraryManagement;
 
 namespace MangaIngestWithUpscaling.RemoteWorker.Background;
 
@@ -14,7 +16,44 @@ public class RemoteTaskProcessor(
             using var scope = serviceScopeFactory.CreateScope();
             var client = scope.ServiceProvider.GetRequiredService<UpscalingService.UpscalingServiceClient>();
 
+            var taskResponse = await client.RequestUpscaleTaskAsync(new Empty(), cancellationToken: stoppingToken);
 
+            if (taskResponse.TaskId == -1)
+            {
+                continue;
+            }
+
+            var profile = GetProfileFromResponse(taskResponse.UpscalerProfile);
         }
+    }
+
+    private static Shared.Data.LibraryManagement.UpscalerProfile GetProfileFromResponse(Api.Upscaling.UpscalerProfile upscalerProfile)
+    {
+        return new Shared.Data.LibraryManagement.UpscalerProfile()
+        {
+            CompressionFormat = upscalerProfile.CompressionFormat switch
+            {
+                Api.Upscaling.CompressionFormat.Webp => Shared.Data.LibraryManagement.CompressionFormat.Webp,
+                Api.Upscaling.CompressionFormat.Png => Shared.Data.LibraryManagement.CompressionFormat.Png,
+                Api.Upscaling.CompressionFormat.Jpg => Shared.Data.LibraryManagement.CompressionFormat.Jpg,
+                Api.Upscaling.CompressionFormat.Avif => Shared.Data.LibraryManagement.CompressionFormat.Avif,
+                _ => throw new InvalidOperationException("Unknown compression format.")
+            },
+            Name = upscalerProfile.Name,
+            Quality = upscalerProfile.Quality,
+            ScalingFactor = upscalerProfile.ScalingFactor switch
+            {
+                Api.Upscaling.ScaleFactor.OneX => Shared.Data.LibraryManagement.ScaleFactor.OneX,
+                Api.Upscaling.ScaleFactor.TwoX => Shared.Data.LibraryManagement.ScaleFactor.TwoX,
+                Api.Upscaling.ScaleFactor.ThreeX => Shared.Data.LibraryManagement.ScaleFactor.ThreeX,
+                Api.Upscaling.ScaleFactor.FourX => Shared.Data.LibraryManagement.ScaleFactor.FourX,
+                _ => throw new InvalidOperationException("Unknown scaling factor.")
+            },
+            UpscalerMethod = upscalerProfile.UpscalerMethod switch
+            {
+                Api.Upscaling.UpscalerMethod.MangaJaNai => Shared.Data.LibraryManagement.UpscalerMethod.MangaJaNai,
+                _ => throw new InvalidOperationException("Unknown upscaler method.")
+            }
+        };
     }
 }
