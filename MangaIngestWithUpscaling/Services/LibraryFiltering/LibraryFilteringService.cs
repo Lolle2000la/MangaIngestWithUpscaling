6a0eee3362859b8f30c569abe6e859780ce8c879
Luca Auer <lolle2000.la@gmail.com>
2025-06-20 23:52:37 +0200

    Fix filtering to be more intuitive

--- MangaIngestWithUpscaling/Services/LibraryFiltering/LibraryFilteringService.cs
+++ MangaIngestWithUpscaling/Services/LibraryFiltering/LibraryFilteringService.cs
@@ -9,33 +9,46 @@ public class LibraryFilteringService : ILibraryFilteringService
 {
     public bool FilterChapter(FoundChapter chapter, IEnumerable<LibraryFilterRule> libraryFilterRules)
     {
-        bool accepted = libraryFilterRules.Select(rule =>
+        List<LibraryFilterRule> rules = libraryFilterRules.ToList();
+        if (!rules.Any())
+        {
+            return true;
+        }
+
+        Func<LibraryFilterRule, bool> checkMatch = rule =>
+        {
+            string? targetFieldData = rule.TargetField switch
             {
-                string? targetFieldData = rule.TargetField switch
-                {
-                    LibraryFilterTargetField.MangaTitle => chapter.Metadata.Series,
-                    LibraryFilterTargetField.ChapterTitle => chapter.Metadata.ChapterTitle,
-                    LibraryFilterTargetField.FilePath => chapter.RelativePath,
-                    _ => throw new NotImplementedException()
-                };
+                LibraryFilterTargetField.MangaTitle => chapter.Metadata.Series,
+                LibraryFilterTargetField.ChapterTitle => chapter.Metadata.ChapterTitle,
+                LibraryFilterTargetField.FilePath => chapter.RelativePath,
+                _ => throw new NotImplementedException()
+            };
 
-                bool ruleMatched = false;
+            if (targetFieldData == null)
+            {
+                return false;
+            }
 
-                if (targetFieldData != null)
-                    ruleMatched = rule.PatternType switch
-                    {
-                        LibraryFilterPatternType.Contains => targetFieldData.Contains(rule.Pattern),
-                        LibraryFilterPatternType.Regex => Regex.IsMatch(targetFieldData, rule.Pattern),
-                        _ => throw new NotImplementedException()
-                    };
+            return rule.PatternType switch
+            {
+                LibraryFilterPatternType.Contains => targetFieldData.Contains(rule.Pattern),
+                LibraryFilterPatternType.Regex => Regex.IsMatch(targetFieldData, rule.Pattern),
+                _ => throw new NotImplementedException()
+            };
+        };
 
-                // If the action is include, then return true if the rule is matched, but if it is exclude,
-                // then return false, since we want want to exclude what was matched here.
-                return rule.Action == FilterAction.Include ? ruleMatched : !ruleMatched;
-            })
-            // Ensure all rules are satisfied.
-            .Aggregate(true, (current, next) => current && next);
+        if (rules.Where(r => r.Action == FilterAction.Exclude).Any(checkMatch))
+        {
+            return false;
+        }
 
-        return accepted;
+        IEnumerable<LibraryFilterRule> includeRules = rules.Where(r => r.Action == FilterAction.Include);
+        if (includeRules.Any() && !includeRules.Any(checkMatch))
+        {
+            return false;
+        }
+
+        return true;
     }
 }
