d45540eddd9fd15117b323faa12a23bba030fff9
Luca Auer <lolle2000.la@gmail.com>
2025-07-03 21:35:15 +0200

    Add a preferred upscaler profile to the Manga entity

---
+++ MangaIngestWithUpscaling/Data/Migrations/20250703193230_AddUpscalerProfilePreference.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MangaIngestWithUpscaling.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpscalerProfilePreference : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UpscalerProfilePreferenceId",
+                table: "MangaSeries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MangaSeries_UpscalerProfilePreferenceId",
+                table: "MangaSeries",
+                column: "UpscalerProfilePreferenceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MangaSeries_UpscalerProfiles_UpscalerProfilePreferenceId",
+                table: "MangaSeries",
+                column: "UpscalerProfilePreferenceId",
+                principalTable: "UpscalerProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MangaSeries_UpscalerProfiles_UpscalerProfilePreferenceId",
+                table: "MangaSeries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MangaSeries_UpscalerProfilePreferenceId",
+                table: "MangaSeries");
+
+            migrationBuilder.DropColumn(
+                name: "UpscalerProfilePreferenceId",
+                table: "MangaSeries");
+        }
+    }
+}
