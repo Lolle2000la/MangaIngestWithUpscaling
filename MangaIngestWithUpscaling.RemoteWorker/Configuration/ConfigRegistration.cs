ec4606ea376c480d04bcac3f1fd49f67542c7040
Luca Auer <lolle2000.la@gmail.com>
2025-09-14 22:14:15 +0200

    Remove `RequiresDynamicCode` and `RequiresUnreferencedCode` attributes since the issue was resolved with a code generator

--- MangaIngestWithUpscaling.RemoteWorker/Configuration/ConfigRegistration.cs
+++ MangaIngestWithUpscaling.RemoteWorker/Configuration/ConfigRegistration.cs
@@ -1,12 +1,9 @@
 ﻿using MangaIngestWithUpscaling.Shared.Configuration;
-using System.Diagnostics.CodeAnalysis;
 
 namespace MangaIngestWithUpscaling.RemoteWorker.Configuration;
 
 public static class ConfigRegistration
 {
-    [RequiresDynamicCode("Configuration binding may require dynamic code at runtime.")]
-    [RequiresUnreferencedCode("Configuration binding may reference members that are trimmed.")]
     public static void RegisterConfig(this WebApplicationBuilder builder)
     {
         builder.Services.Configure<UpscalerConfig>(builder.Configuration.GetSection(UpscalerConfig.Position));
