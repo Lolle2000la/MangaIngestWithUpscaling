a01aa971fa34149dc2851623716b88065554fe85
Luca Auer <lolle2000.la@gmail.com>
2025-12-20 23:33:31 +0100

    Add composite index on Status and CreatedAt for PersistedTasks

---
+++ MangaIngestWithUpscaling/Data/Migrations/20251220223242_AddIndexForPersistedTask.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MangaIngestWithUpscaling.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexForPersistedTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PersistedTasks_Status_CreatedAt",
+                table: "PersistedTasks",
+                columns: new[] { "Status", "CreatedAt" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PersistedTasks_Status_CreatedAt",
+                table: "PersistedTasks");
+        }
+    }
+}
